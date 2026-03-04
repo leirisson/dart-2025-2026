daa44e6eb212e4fc8f46a00755150937d3f1f002
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-04 17:45:00 -0400

    Refactor input handling for calculator operations and improve code structure

--- 3-alura-criando-um-projeto-com-dart/calculadora_1/bin/calculadora_1.dart
+++ 3-alura-criando-um-projeto-com-dart/calculadora_1/bin/calculadora_1.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
 void main() {
-  print("Informe o primeiro valor: ");
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  print("informe o segundo valor: ");
-  double numerodois = double.parse(stdin.readLineSync()!);
+  double numeroUm = 0;
+  double numerodois = 0;
+  String? entrada = "";
+  String operacao = "";
+  List<String> operacoes = <String>["+", "-", "/", "*"];
 
   void soma() {
     print(numeroUm + numerodois);
@@ -22,22 +23,79 @@ void main() {
     print(numeroUm * numerodois);
   }
 
+  void getOperacao() {
+    print("Digite uma opração: ");
+    entrada = stdin.readLineSync()!;
+    if (entrada != null) {
+      if (operacoes.contains(entrada)) {
+        operacoes = entrada as List<String>;
+      }
+    }
+  }
+
   print("Escolha uma opção: ");
   print("1. soma");
   print("2. subtrção");
   print("3. divisão");
   print("4. muitiplicação");
-
-  String operacao = stdin.readLineSync()!;
+  operacao = stdin.readLineSync()!;
 
   switch (operacao) {
     case "1":
+      print("digite o primeiro valor: ");
+      entrada = stdin.readLineSync()!;
+
+      if (entrada != "") {
+        numeroUm = double.parse(entrada!);
+      }
+
+      print("digite o segundo valor: ");
+      entrada = stdin.readLineSync()!;
+      if (entrada != "") {
+        numerodois = double.parse(entrada!);
+      }
       soma();
     case "2":
+      print("digite o primeiro valor: ");
+      entrada = stdin.readLineSync()!;
+
+      if (entrada != "") {
+        numeroUm = double.parse(entrada!);
+      }
+
+      print("digite o segundo valor: ");
+      entrada = stdin.readLineSync()!;
+      if (entrada != "") {
+        numerodois = double.parse(entrada!);
+      }
       subtracao();
     case "3":
+      print("digite o primeiro valor: ");
+      entrada = stdin.readLineSync()!;
+
+      if (entrada != "") {
+        numeroUm = double.parse(entrada!);
+      }
+
+      print("digite o segundo valor: ");
+      entrada = stdin.readLineSync()!;
+      if (entrada != "") {
+        numerodois = double.parse(entrada!);
+      }
       divisao();
     case "4":
+      print("digite o primeiro valor: ");
+      entrada = stdin.readLineSync()!;
+
+      if (entrada != "") {
+        numeroUm = double.parse(entrada!);
+      }
+
+      print("digite o segundo valor: ");
+      entrada = stdin.readLineSync()!;
+      if (entrada != "") {
+        numerodois = double.parse(entrada!);
+      }
       mutiplicacao();
       break;
   }
