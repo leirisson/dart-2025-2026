3c0b03b9e0f73d157cb43cfb03330fda441cb9b4
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-04 15:49:35 -0400

    Refactor calculator functions for improved readability and structure

--- 3-alura-criando-um-projeto-com-dart/calculadora_1/bin/calculadora_1.dart
+++ 3-alura-criando-um-projeto-com-dart/calculadora_1/bin/calculadora_1.dart
@@ -6,40 +6,39 @@ void main() {
   print("informe o segundo valor: ");
   double numerodois = double.parse(stdin.readLineSync()!);
 
+  void soma() {
+    print(numeroUm + numerodois);
+  }
 
- void soma(){
-  print(numeroUm + numerodois);
- }
+  void subtracao() {
+    print(numeroUm - numerodois);
+  }
 
- void subtracao(){
-  print(numeroUm - numerodois);
- }
+  void divisao() {
+    print(numeroUm / numerodois);
+  }
 
- void divisao(){
-  print(numeroUm / numerodois);
- }
+  void mutiplicacao() {
+    print(numeroUm * numerodois);
+  }
 
- void mutiplicacao(){
-  print(numeroUm * numerodois);
- }
+  print("Escolha uma opção: ");
+  print("1. soma");
+  print("2. subtrção");
+  print("3. divisão");
+  print("4. muitiplicação");
 
- print("Escolha uma opção: ");
- print("1. soma");
- print("2. subtrção");
- print("3. divisão");
- print("4. muitiplicação");
+  String operacao = stdin.readLineSync()!;
 
- String operacao = stdin.readLineSync()!;
-
- switch(operacao){
-  case "1":
-    soma();
-  case "2":
-    subtracao();
-  case "3":
-    divisao();
-  case "4":
-    mutiplicacao();
- }
-
+  switch (operacao) {
+    case "1":
+      soma();
+    case "2":
+      subtracao();
+    case "3":
+      divisao();
+    case "4":
+      mutiplicacao();
+      break;
+  }
 }
