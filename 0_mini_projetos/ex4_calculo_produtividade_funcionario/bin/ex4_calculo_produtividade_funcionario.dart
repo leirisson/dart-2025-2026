84cf5ca0f67d80e143f42d4b8c0c6405b79008d8
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-04 23:09:00 -0400

    feat: enhance productivity calculation program with detailed comments and structure

--- 0_mini_projetos/ex4_calculo_produtividade_funcionario/bin/ex4_calculo_produtividade_funcionario.dart
+++ 0_mini_projetos/ex4_calculo_produtividade_funcionario/bin/ex4_calculo_produtividade_funcionario.dart
@@ -1,5 +1,40 @@
-import 'package:ex4_calculo_produtividade_funcionario/ex4_calculo_produtividade_funcionario.dart' as ex4_calculo_produtividade_funcionario;
+// Exercício 4 — Cálculo de Produtividade do Funcionário
+// Uma empresa quer medir produtividade.
+// 📌 O funcionário informa:
+// Quantidade de tarefas concluídas
+// Total de horas trabalhadas
+// 🎯 O sistema deve calcular:
+// Média de tarefas por hora
+// Total estimado de tarefas se ele trabalhasse 160 horas no mês
 
-void main(List<String> arguments) {
-  print('Hello world: ${ex4_calculo_produtividade_funcionario.calculate()}!');
+/**
+ * EXPLICAÇÃO DO CODIGO
+ * 1. VOU DECLARAR AS VARIAVEIS RESPONSAVEIS POR AMAZEBAR AS
+ * HORAS TRABALHADAS E TAREFAS CONCLUIDAS
+ * 2.CAPTURAR OS DADOS DO USUASRIO
+ * 4. CALCULAR A MÉDIA DE TAREFAS POR HORA
+ * 5. EXIBIR O RESUDATO NO TERMINAL.
+ */
+
+import 'dart:io';
+
+void main() {
+  int tarefasConcluidas = 0;
+  double totoalDeHorasTrabalhadas = 0;
+  double mediaTarefasPorHoras = 0;
+  double estimativa = 0;
+
+  print("quantas tarefas foram concluidas ?:");
+  tarefasConcluidas = int.parse(stdin.readLineSync()!);
+
+  print("quntas horas você gastou ?: ");
+  totoalDeHorasTrabalhadas = double.parse(stdin.readLineSync()!);
+
+  mediaTarefasPorHoras = tarefasConcluidas / totoalDeHorasTrabalhadas;
+  estimativa = mediaTarefasPorHoras * 60;
+
+  print("Média por hora: $mediaTarefasPorHoras");
+  print("Estimativa para 160h: $estimativa");
+
+
 }
