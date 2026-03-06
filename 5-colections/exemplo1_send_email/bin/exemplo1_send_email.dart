0aec25222fcc3a669d972d30ff393388b8d5467e
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-06 11:24:18 -0400

    feat: implement email sending functionality in main method

--- 5-colections/exemplo1_send_email/bin/exemplo1_send_email.dart
+++ 5-colections/exemplo1_send_email/bin/exemplo1_send_email.dart
@@ -1,7 +1,12 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  List<String> listaDeEmails = ["leirisson@gmail.com", "maria@gmail.com"];
 
 
-
+  
+  for (String email in listaDeEmails) {
+    sendEmail(email);
+  }
+}
 
 void sendEmail(String email) {
   print("mensagem enviada para: $email");
