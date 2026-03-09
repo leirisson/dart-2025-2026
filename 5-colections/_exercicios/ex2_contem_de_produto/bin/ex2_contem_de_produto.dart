87e9736ea2af21a889c58b5e16f2260e9522a1e3
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-09 09:12:17 -0400

    execicios de logica com map, set, List

--- 5-colections/_exercicios/ex2_contem_de_produto/bin/ex2_contem_de_produto.dart
+++ 5-colections/_exercicios/ex2_contem_de_produto/bin/ex2_contem_de_produto.dart
@@ -1,5 +1,32 @@
-import 'package:ex2_contem_de_produto/ex2_contem_de_produto.dart' as ex2_contem_de_produto;
+void main() {
+  final produtos = [
+    {"nome": "Notebook", "categoria": "eletronico"},
+    {"nome": "Mouse", "categoria": "eletronico"},
+    {"nome": "Teclado", "categoria": "eletronico"},
+    {"nome": "Cadeira", "categoria": "moveis"},
+    {"nome": "Mesa", "categoria": "moveis"},
+    {"nome": "Caneta", "categoria": "papelaria"},
+  ];
 
-void main(List<String> arguments) {
-  print('Hello world: ${ex2_contem_de_produto.calculate()}!');
+  contagemProdutosPorCategoria(produtos);
+}
+
+// indentificar categorias unicas
+
+Map<String, int> contagemProdutosPorCategoria(
+  List<Map<String, String>> produtos,
+) {
+  Map<String, int> qtdProCategoria = {};
+  Set<String> cat = {};
+
+  int contador = 0;
+  int indexCategoria = 0;
+
+  for (var produto in produtos) {
+    cat.add(produto['categoria']!);
+  }
+
+
+
+  return qtdProCategoria;
 }
