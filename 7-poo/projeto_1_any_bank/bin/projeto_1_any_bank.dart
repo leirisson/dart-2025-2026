c5c041f3d02fcce3f36c9af29041ac1c182881eb
leirisson <103783610+leirisson@users.noreply.github.com>
2026-03-09 10:02:03 -0400

    feta: criando funções de tranferencia de valores

--- 7-poo/projeto_1_any_bank/bin/projeto_1_any_bank.dart
+++ 7-poo/projeto_1_any_bank/bin/projeto_1_any_bank.dart
@@ -1,18 +1,38 @@
 void main(List<String> arguments) {
-  List<String> contas = <String>["Matheus", "Roberta"];
-  List<double> saldos = <double>[100, 100];
-
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
-  
+
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
+
+  for (Conta conta in contas) {
+    print("");
+    print("=============================");
+    print(conta.titular);
+    print(conta.saldo);
+  }
+
+  receber(contaMatheus, 500);
+  print(contaMatheus.saldo);
+
+  enviarDinheiro(contaMatheus, 150);
+  print(contaMatheus.saldo);
+}
+
+
+
+void receber(Conta conta, double valor){
+  conta.saldo += valor;
+}
+
+void enviarDinheiro(Conta contaOrigem, double valor){
+  contaOrigem.saldo -= valor;
 }
 
 class Conta {
-  String titular ;
+  String titular;
   double saldo;
 
   Conta(this.titular, this.saldo);
 
+
 }
-
-
