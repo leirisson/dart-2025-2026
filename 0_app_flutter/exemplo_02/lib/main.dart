5d55498f764614ec459bee567591166bcd9eb49a
leirisson <103783610+leirisson@users.noreply.github.com>
2026-05-26 15:22:01 -0400

    Add Windows runner support for Flutter application
    - Create a manifest file for the runner executable to enable DPI awareness and compatibility with Windows 10 and 11.
    - Implement utility functions in utils.cpp and utils.h for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion.
    - Develop Win32Window class in win32_window.cpp and win32_window.h to manage window creation, message handling, and theme updates based on system preferences.
    - Ensure proper DPI scaling and dark mode support for the application window.

---
+++ 0_app_flutter/exemplo_02/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(Container(
+    child: Center(
+      child: Text(
+        "Leirisson",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.white, fontSize: 45),
+        )
+    ),
+  ));
+}
