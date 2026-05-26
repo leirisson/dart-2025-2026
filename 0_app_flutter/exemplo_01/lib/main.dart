60b60c28dfb6ec70db7efb09be22074154e45089
leirisson <103783610+leirisson@users.noreply.github.com>
2026-05-26 15:03:44 -0400

    feat: implement question and answer components for quiz app

--- 0_app_flutter/exemplo_01/lib/main.dart
+++ 0_app_flutter/exemplo_01/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
+import './resposta.dart';
 
 void main() => runApp(PerguntaApp());
 
@@ -9,24 +11,57 @@ class _PerguntaAppState extends State<PerguntaApp> {
     setState(() {
       perguntaSelecionada++;
     });
-    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      "Qual a sua cor favorita ?",
-      "Qual o nome do seu cachorro ?",
+    final List<Map<String, Object>> _perguntas = [
+      {
+        'texto': "Qual a sua cor favorita ?",
+        'resposta': ['azul', 'verde', 'amarelo'],
+      },
+      {
+        'texto': "Qual o nome do seu animal ?",
+        'resposta': ['coelho', 'elefante', 'girafa'],
+      },
+      {
+        'texto': "Qual o seu esporte favorito ?",
+        'resposta': ['futebol', 'basquete', 'natação'],
+      },
+      {
+        'texto': "Qual a sua fruta favorita ?",
+        'resposta': ['manga', 'banana', 'morango'],
+      },
+      {
+        'texto': "Qual o seu meio de transporte favorito ?",
+        'resposta': ['carro', 'moto', 'bicicleta'],
+      },
+      {
+        'texto': "Qual a sua estação do ano favorita ?",
+        'resposta': ['verão', 'inverno', 'primavera'],
+      },
+      {
+        'texto': "Qual o seu hobby favorito ?",
+        'resposta': ['leitura', 'jogos', 'culinária'],
+      },
     ];
+
+    // Constrói dinamicamente os botões de resposta da pergunta atual
+    // List<Widget> respostas = [];
+    // for(String textoResp in _perguntas[perguntaSelecionada]['resposta'] as List){
+    //   respostas.add(Resposta(textoResp, _responder));
+    // }
+
+    List<String> respostas = _perguntas[perguntaSelecionada]['resposta'] as List<String>;
+
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("App de perguntas")),
         body: Column(
           children: [
-            Text(perguntas[perguntaSelecionada]),
-            ElevatedButton(onPressed: _responder, child: Text("Resposta 1")),
-            ElevatedButton(onPressed: _responder, child: Text("Resposta 2")),
+            Questao(_perguntas[perguntaSelecionada]['texto'] as String),
+            ...respostas.map((texto) => Resposta(texto, _responder)),
           ],
         ),
       ),
